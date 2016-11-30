49c2a5356cc3e5c7b808352f78c4cb888d3cd3e1
BrandonCao <brandonnhatcao@gmail.com>
2016-11-30 15:15:40 -0800

    added bootstrap, routing works

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'hello world'
+    return render_template('index.html') 
 
+@app.route('/results', methods=['POST'])
+def showResults():
+	query = request.form['query']
+	return render_template('results.html', query=query)
 
 if __name__ == '__main__':
-    app.run(host='ix.cs.uoregon.edu',port=8080, debug=True)
+    app.run(port=8080, debug=True)
