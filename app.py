317c9e1c1ce72abb9b097bd59db1a894db40ba3b
BrandonCao <brandonnhatcao@gmail.com>
2016-12-06 02:37:55 -0800

    cleaned up code

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from flask import Flask, render_template, request, url_for, jsonify, json,redirect
 from dbApp import config
 from dbApp.db import *
-from dbApp.functions import *
 
 
 app = Flask(__name__)
+dbFunctions = dbApp.db.dbFunctions()
 
 @app.route('/')
 def hello():
     return render_template('index.html') 
 
+@app.route('/', methods=['GET','POST'])
+def redirectPage():
+	option = str(request.form.get("options"))
+	return redirect('/'+option)
 
 @app.route('/<page>Result', methods=['POST'])
 def results(page):
@@ -17,50 +21,42 @@ def results(page):
 		teamId = request.form['options']
 		year = request.form['years']
 		columns = request.form.getlist("columnValue")
-		results = dbApp.functions.getTeamPerformance(columns,teamId,year)
+		results = dbFunctions.getTeamPerformance(columns,teamId,year)
 		return render_template('results.html',result=results) 
 	elif page == 'customQuery':
 		query = request.form['query']
-		results = dbApp.db.query(query)
+		results = dbFunctions.query(query)
 		results.append(query)
 		return render_template('results.html',result=results) 
 	elif page == 'teamInfo':
 		teamId = request.form['options']
-		results = dbApp.functions.getTeamInfo(teamId)
+		results = dbFunctions.getTeamInfo(teamId)
 		return render_template('results.html',result=results) 
 	elif page == 'playerTeamPerformance':
 		teamId = request.form['team']
 		posId = request.form['pos']
 		table = request.form['table']
-		print(teamId,posId,table)
-		results = dbApp.functions.playerTeamPerformance(int(teamId),str(posId),str(table),2016)
+		results = dbFunctions.playerTeamPerformance(int(teamId),str(posId),str(table),2016)
 		return render_template('results.html',result=results) 
 
-@app.route('/', methods=['GET','POST'])
-def showPage():
-	option = str(request.form.get("options"))
-	print(option)
-	return redirect(url_for(option))
-
-
-@app.route('/team')
+@app.route('/Team-Performance')
 def teamPerf():
-	teams = dbApp.functions.getAllTeams()
+	teams = dbFunctions.getAllTeams()
 	return render_template('team.html',teams=teams)
 
-@app.route('/teamInfo')
+@app.route('/Team-Information')
 def teamInformation():
-	teams = dbApp.functions.getAllTeams()
+	teams = dbFunctions.getAllTeams()
 	return render_template('teamInfo.html',teams=teams)
 
-@app.route('/custom')
+@app.route('/Custom-Query')
 def customQuery():
 	return render_template('customQuery.html')
 
-@app.route('/playersByTeam')
+@app.route('/Player-By-Team')
 def playersByTeam():
-	teams = dbApp.functions.getAllTeams()
-	positions = dbApp.functions.getAllPositions()
+	teams = dbFunctions.getAllTeams()
+	positions = dbFunctions.getAllPositions()
 	return render_template('players.html', positions = positions,teams=teams)
 
 if __name__ == '__main__':
--- dbApp/db.py
+++ dbApp/db.py
@@ -2,7 +2,7 @@ import dbApp.config
 import mysql.connector
 from mysql.connector import errorcode, errors
 from datetime import date, datetime, timedelta
-
+	
 configDB = {	
 	'user': dbApp.config.DBuser,
 	'password': dbApp.config.DBpassword,
@@ -13,30 +13,89 @@ configDB = {
 	'autocommit': dbApp.config.DBautocommit# ,
 }
 
-#Custom query
-def query(query='', parameters=()):
-	cnx = mysql.connector.connect(**configDB)
-	print(query)
-	print(parameters)
-	if cnx.is_connected():
-		print('database connection established.')
-		pass
-	else:
-		print('database connection failed.')
-	cursor = cnx.cursor()
-	cursor.execute(query, parameters)
-	columns = [desc[0] for desc in cursor.description]
-	columnValues = cursor.fetchall()
-	if len(columnValues) == 0:
-		return []
-	else:
-		values = []
-		for row in columnValues:
-			row = dict(zip(columns, row))
-			values.append(row)
-		result = []
-		result.append(columns)
-		result.append(values)
-		cnx.close()
+
+class dbFunctions:
+	#Custom query
+	def query(self, query='', parameters=()):
+		cnx = mysql.connector.connect(**configDB)
+		print(query)
+		print(parameters)
+		if cnx.is_connected():
+			print('database connection established.')
+			pass
+		else:
+			print('database connection failed.')
+		cursor = cnx.cursor()
+		cursor.execute(query, parameters)
+		columns = [desc[0] for desc in cursor.description]
+		columnValues = cursor.fetchall()
+		if len(columnValues) == 0:
+			return []
+		else:
+			values = []
+			for row in columnValues:
+				row = dict(zip(columns, row))
+				values.append(row)
+			result = []
+			result.append(columns)
+			result.append(values)
+			cnx.close()
+			return result
+
+	#Returns list of all teams 
+	def getAllTeams(self):
+		cnx = mysql.connector.connect(**configDB)
+		string = 'SELECT CONCAT(t.location, " ", t.team_name), t.team_id FROM NFLDB.Teams t'
+		if cnx.is_connected():
+			print('database connection established.')
+			pass
+		else:
+			print('database connection failed.')
+		cursor = cnx.cursor()
+		cursor.execute(string)
+		result = cursor.fetchall()
 		return result
 
+	#Returns list of all positions 
+	def getAllPositions(self):
+		cnx = mysql.connector.connect(**configDB)
+		string = 'SELECT pos_id, position_name FROM NFLDB.positions'
+		if cnx.is_connected():
+			print('database connection established.')
+			pass
+		else:
+			print('database connection failed.')
+		cursor = cnx.cursor()
+		cursor.execute(string)
+		result = cursor.fetchall()
+		return result
+
+	#Get results for team performance search
+	def getTeamPerformance(self,columns, team, year):
+		statement = 'SELECT CONCAT(t.location, " ", t.team_name) as "Team Name", ' + ','.join([str(x) for x in columns]) + ' FROM NFLDB.team_season_performance s LEFT JOIN NFLDB.Teams t ON t.team_id = s.team_id WHERE season_id = '+ year + ' AND t.team_id = ' + team
+		result = self.query(statement)
+		result.append(statement)
+		return result
+
+	#Get results for team information search
+	def getTeamInfo(self,team):
+		string = "SELECT t.location, t.team_name, CONCAT(o.fname, ' ', o.lname) as 'Owner', l.league_name,CONCAT(c.fname, ' ', c.lname) as 'Coach' FROM NFLDB.Teams t LEFT JOIN NFLDB.owners o on o.owner_id = t.owner_id LEFT JOIN NFLDB.League  l on l.league_id = t.league_id LEFT JOIN NFLDB.Coach c ON c.coach_id = t.coach_id WHERE t.team_id = "+team;
+		result = self.query(string)
+		result.append(string)
+		return result
+
+
+	#Get results for player performance by team
+	def playerTeamPerformance(self,team,position,table,season):
+		string = "SELECT CONCAT(pl.fname, ' ' ,pl.lname) as 'Name', CONCAT(t.location, ' ', t.team_name) as 'Team Name', p.* FROM %s p JOIN NFLDB.Players pl ON p.player_id = pl.player_id JOIN NFLDB.Teams t on pl.team_id = t.team_id WHERE pl.player_id in (SELECT player_id FROM NFLDB.Players WHERE team_id = %i ) AND p.season_id = %i"
+		if position != "0":
+			string += " AND pl.position_id = '%s'"
+			result = self.query(string % (table, team, season, position))
+			string = string % (table, team, season, position)
+		else:
+			result = self.query(string % (table, team, season))
+			string = string % (table, team, season)
+		if len(result) == 0:
+			result.append([])
+		result.append(string)
+		return result
